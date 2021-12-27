3b53ad9c0371994055fd0f83faaa7052dca86f4b
alexander-lam <81650765+alexander-lam@users.noreply.github.com>
2021-12-26 23:50:33 -0500

    Add comments

--- Day6/Day6.py
+++ Day6/Day6.py
@@ -12,20 +12,26 @@ start = time.time()
 # Read input file
 input = open('input1.txt', 'r').read().split(',')
 
+# Instantiate cycle and number of days
 cycle = {0:0, 1:0, 2:0, 3:0, 4:0, 5:0, 6:0, 7:0, 8:0}
 days = 256
 
+# Fill in cycle
 for element in input:
 	cycle[int(element)] += 1
 
 for i in range(days):
+	# Load into new dictionary, decreasing keys by one
 	newCycle = {}
 	for j in range(8):
 		newCycle[j] = cycle[j+1]
+	# When lanternfish duplicate, add new ones to 6 days and 8 days
 	newCycle[6] += cycle[0]
 	newCycle[8] = cycle[0]
+	# Reset
 	cycle = newCycle
 
+# Compute sum of all lanternfish
 print(sum(cycle.values()))
 
 # Stop timer
